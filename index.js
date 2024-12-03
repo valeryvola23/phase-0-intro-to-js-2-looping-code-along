108d33ea8f77690bc9fa998c5ebd21c9f2b6c1a8
Valeryvola <valeryvola@yahoo.com>
2024-12-03 13:10:40 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+  const names = ["Guadalupe", "Ollie", "Aki"];
+  const event = "birthday";
+  function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+function countDown(start) {
+    while (start >= 0) {
+        console.log(start);
+        start--; 
+    }
+}
